b43c7a1b6e38ad47423c2971797742a6b08ddf1f
Sultanxda <sultanxda@gmail.com>
2016-07-16 21:58:45 -0700

    misc: fpc1020: Ignore home key presses when touchscreen is in use
    When the user's finger is on the touchscreen, don't process any home key
    button presses, as an unintended button press will cause poor UX.
    
    Ignore home key presses when the Synaptics s1302 driver is ignoring keypad
    presses.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s1302.c
+++ drivers/input/touchscreen/synaptics_driver_s1302.c
@@ -1950,6 +1950,11 @@ static int choice_gpio_function(struct synaptics_ts_data *ts)
 		ret = i2c_smbus_write_byte_data(ts->client, 0x37, 0x80);
 	}
 	return ret;
+bool s1302_is_keypad_stopped(void)
+{
+	struct synaptics_ts_data *ts = tc_g;
+
+	return ts->stop_keypad;
 }
 
 static void synaptics_input_event(struct input_handle *handle,
